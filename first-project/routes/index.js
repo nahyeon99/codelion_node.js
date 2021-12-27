d98213146ccdebd3f2609bd90e4272db9866d405
NaHyeon <nahyeonee99@gmail.com>
2021-12-27 16:18:36 +0900

    미들웨어

--- first-project/routes/index.js
+++ first-project/routes/index.js
@@ -1,46 +1,10 @@
 var express = require("express");
 var router = express.Router();
+const loginCheck = require("../module/loginCheck");
 
-let arr = [];
-
-// GET method
-router.get("/read", (req, res) => {
+router.get("/", loginCheck, (req, res) => {
   res.status(200).json({
-    message: "read success",
-  });
-});
-
-// POST method
-
-router.post("/create", (req, res) => {
-  // 비구조화 할당, 해당 데이터를 바로 받아올 수 있음
-  const { data } = req.body;
-  arr.push(data);
-  res.status(200).json({
-    message: "create success",
-    result: arr,
-  });
-});
-
-// PUT method
-
-router.put("/update/:id", (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-  arr[id] = data;
-  res.status(200).json({
-    message: "update success",
-    result: arr,
-  });
-});
-
-// DELETE method
-router.delete("/delete/:id", (req, res) => {
-  const { id } = req.params;
-  arr.splice(id, 1);
-  res.status(200).json({
-    message: "delete success",
-    result: arr,
+    message: "login success!",
   });
 });
 
