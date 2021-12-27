9020d041caae34c68bcf22dd15396ad1c24ea678
NaHyeon <nahyeonee99@gmail.com>
2021-12-27 15:08:57 +0900

    express-http method

--- first-project/routes/index.js
+++ first-project/routes/index.js
@@ -1,19 +1,47 @@
 var express = require("express");
 var router = express.Router();
 
-// router.get("/", (req, res) => {
-// res.json({
-//   message: "main success!!",
-// });
-// });
+let arr = [];
 
-router.post("/main", (req, res) => {
-  const data = req.body.data;
-  // res.send("문자열이 응답됩니다.");
-  // res.json({
-  //   message: "json 응답"
-  // })
-  res.render("index");
+// GET method
+router.get("/read", (req, res) => {
+  res.status(200).json({
+    message: "read success",
+  });
+});
+
+// POST method
+
+router.post("/create", (req, res) => {
+  // 비구조화 할당, 해당 데이터를 바로 받아올 수 있음
+  const { data } = req.body;
+  arr.push(data);
+  res.status(200).json({
+    message: "create success",
+    result: arr,
+  });
+});
+
+// PUT method
+
+router.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  arr[id] = data;
+  res.status(200).json({
+    message: "update success",
+    result: arr,
+  });
+});
+
+// DELETE method
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  arr.splice(id, 1);
+  res.status(200).json({
+    message: "delete success",
+    result: arr,
+  });
 });
 
 module.exports = router;
